efb44d57c8cce44de923c84c64b5b563544441c1
fanjiawei <fanjw@fengyuntec.com>
2017-09-04 15:36:30 +0800

    日志信息级别配置
    debug > info > warn > error

--- src/main/java/com/me/jovi/Application.java
+++ src/main/java/com/me/jovi/Application.java
@@ -1,5 +1,7 @@
 package com.me.jovi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     @RequestMapping("/")
     String home() {
+        LOGGER.debug("这是一个 debug 消息");
+        LOGGER.info("这是一个 info 消息");
+        LOGGER.warn("这是一个 warn 消息");
+        LOGGER.error("这是一个 error 消息");
         return "Hello World!";
     }
     public static void main(String[] args) throws Exception {
